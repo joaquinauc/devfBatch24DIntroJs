c89c0ac62015cdd3b6349fe19404f4b701951d6f
joaquinauc <joaquins_urrea@hotmail.com>
2023-03-26 20:59:50 -0700

    Tarea de ejercicios de array

---
+++ src/tarea/array/ejercicio-2.js
@@ -0,0 +1,5 @@
+let palabras = prompt("Ingrese algunas palabras o numeros separas por una ',': ");
+
+let arr = palabras.split(",")
+
+console.log(arr);
---
+++ src/tarea/array/ejercicio-3.js
@@ -0,0 +1,5 @@
+let arr = [10,40,30,20,15,5];
+
+arr.sort(function(a, b){return a - b});
+
+console.log(arr);
